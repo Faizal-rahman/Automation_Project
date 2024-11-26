7264101e01d0b8435e5b418fd94c2ad83b4df601
Smriti Banjade <smritibanjade71@gmail.com>
2024-11-25 22:07:44 -0500

    Separated auto scaling group and load balancer

--- application/dev/webservers/main.tf
+++ application/dev/webservers/main.tf
@@ -47,127 +47,6 @@ locals {
 module "globalvars" {
   source = "../../../modules/globalvars"
 }
-# Load balancer
-resource "aws_elb" "web_elb" {
-  name = "web-${var.env}-elb"
-  security_groups = [
-    "${aws_security_group.public_sg.id}"
-  ]
-
-  subnets = data.terraform_remote_state.network.outputs.public_subnet_id
-
-  cross_zone_load_balancing = true
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    target              = "HTTP:80/"
-  }
-  listener {
-    lb_port           = 80
-    lb_protocol       = "http"
-    instance_port     = "80"
-    instance_protocol = "http"
-  }
-}
-
-resource "aws_launch_configuration" "web" {
-  name_prefix                 = "web--${var.env}-"
-  image_id                    = data.aws_ami.latest_amazon_linux.id
-  instance_type               = lookup(var.instance_type, var.env)
-  key_name                    = aws_key_pair.web_key.key_name
-  security_groups             = ["${aws_security_group.elastic_sg.id}"]
-  associate_public_ip_address = true
-  user_data = templatefile("${path.module}/install_httpd.sh.tpl",
-    {
-      env    = upper(var.env),
-      prefix = upper(local.prefix)
-    }
-  )
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-# Auto Scaling
-resource "aws_autoscaling_group" "web" {
-  name             = "${aws_launch_configuration.web.name}-asg"
-  min_size         = 1
-  desired_capacity = 2
-  max_size         = 4
-
-  health_check_type = "ELB"
-  load_balancers = [
-    "${aws_elb.web_elb.id}"
-  ]
-  launch_configuration = aws_launch_configuration.web.name
-  enabled_metrics = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupTotalInstances"
-  ]
-  metrics_granularity = "1Minute"
-  vpc_zone_identifier = data.terraform_remote_state.network.outputs.public_subnet_id
-
-  # Required to redeploy without an outage.-
-  lifecycle {
-    create_before_destroy = true
-  }
-  tag {
-    key                 = "Name"
-    value               = "web-${var.env}"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_autoscaling_policy" "web_policy_up" {
-  name                   = "web_policy_up"
-  scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-}
-resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
-  alarm_name          = "web_cpu_alarm_up"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "70"
-  dimensions = {
-    AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
-  }
-  alarm_description = "This metric monitor EC2 instance CPU utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.web_policy_up.arn}"]
-}
-resource "aws_autoscaling_policy" "web_policy_down" {
-  name                   = "web_policy_down"
-  scaling_adjustment     = -1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-}
-resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
-  alarm_name          = "web_cpu_alarm_down"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "30"
-  dimensions = {
-    AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
-  }
-  alarm_description = "This metric monitor EC2 instance CPU utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.web_policy_down.arn}"]
-}
-
 
 # Reference subnet provisioned
 resource "aws_instance" "public_vms" {
@@ -274,7 +153,6 @@ resource "aws_instance" "bastion" {
     }
   }
 
-
   tags = merge(local.default_tags,
     {
       "Name" = "${local.name_prefix}-Amazon-Linux-bastion"
@@ -308,73 +186,6 @@ resource "aws_security_group" "bastion_sg" {
   tags = merge(local.default_tags,
     {
       "Name" = "${local.name_prefix}-bastion-sg"
-    }
-  )
-}
-
-# Creating Security Group for ELB
-resource "aws_security_group" "public_sg" {
-  name        = "Public Security Group"
-  description = "Public Security Group"
-  vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
-  # Inbound Rules
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # HTTPS access from anywhere
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # Outbound Rules
-  # Internet access to anywhere
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.default_tags,
-    {
-      "Name" = "${local.name_prefix}-public-sg"
-    }
-  )
-}
-
-resource "aws_security_group" "elastic_sg" {
-  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.public_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.default_tags,
-    {
-      "Name" = "${local.name_prefix}-elastic-sg"
     }
   )
 }
