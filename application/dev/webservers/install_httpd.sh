6ee0a9ffc19d4327e95c1bf965f9b68ab0495d54
Faizal Rahman <96905693+Faizal-rahman@users.noreply.github.com>
2024-11-27 20:56:37 -0500

    displays image

--- application/dev/webservers/install_httpd.sh
+++ application/dev/webservers/install_httpd.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+# Update and install Apache HTTP server
 yum -y update
 yum -y install httpd
-myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1>Welcome to ACS730 final project ! My private IP is $myip</h1><br>Built by Terraform! prepared by Smriti Banjade , Faizal Rahman Saffiudeen , Reham Garaween and Esha Nikhil Chawda"  >  /var/www/html/index.html
+
+# Fetch the private IP of the instance
+myip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# Create the HTML file in the web server directory
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>${myip}</title>
+</head>
+<body>
+  <h1>Group Members</h1>
+  <ul>
+    <li>Faizal Rahman Saffiudeen</li>
+    <li>Reham Garaween</li>
+    <li>Smriti Banjadee</li>
+    <li>Esha Nikhil Chawda</li>
+  </ul>
+  <img src="https://s3.amazonaws.com/your-bucket-name/image.jpg" alt="Group Photo">
+</body>
+</html>
+EOF
+
+# Start and enable the Apache HTTP server
 sudo systemctl start httpd
 sudo systemctl enable httpd
