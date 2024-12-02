5905ac8f88d0d4e7c6244f39d11d48ea6d71d8c0
EC2 Default User <ec2-user@ip-172-31-94-147.ec2.internal>
2024-12-02 04:45:21 +0000

    Prod cleanup

--- application/network/output.tf
+++ application/network/output.tf
@@ -1,21 +1,21 @@
 output "vpc_id" {
-  value = module.vpc_dev.vpc_id
+  value = module.vpc_prod.vpc_id
 }
 
 output "public_subnet_id" {
-  value = module.vpc_dev.public_subnet_id
+  value = module.vpc_prod.public_subnet_id
 }
 
 output "private_subnet_id" {
-  value = module.vpc_dev.private_subnet_id
+  value = module.vpc_prod.private_subnet_id
 }
 
 output "public_route_tables" {
-  value = module.vpc_dev.public_route_tables
+  value = module.vpc_prod.public_route_tables
 }
 
 output "private_route_tables" {
-  value = module.vpc_dev.private_route_tables
+  value = module.vpc_prod.private_route_tables
 }
 
 output "vpc_cidr" {
