2083cc51c1f83e7f65193248df8a666de5510d89
EC2 Default User <ec2-user@ip-172-31-94-147.ec2.internal>
2024-12-02 05:10:17 +0000

    staging cleanup

--- application/network/output.tf
+++ application/network/output.tf
@@ -1,21 +1,21 @@
 output "vpc_id" {
-  value = module.vpc_dev.vpc_id
+  value = module.vpc_stage.vpc_id
 }
 
 output "public_subnet_id" {
-  value = module.vpc_dev.public_subnet_id
+  value = module.vpc_stage.public_subnet_id
 }
 
 output "private_subnet_id" {
-  value = module.vpc_dev.private_subnet_id
+  value = module.vpc_stage.private_subnet_id
 }
 
 output "public_route_tables" {
-  value = module.vpc_dev.public_route_tables
+  value = module.vpc_stage.public_route_tables
 }
 
 output "private_route_tables" {
-  value = module.vpc_dev.private_route_tables
+  value = module.vpc_stage.private_route_tables
 }
 
 output "vpc_cidr" {
